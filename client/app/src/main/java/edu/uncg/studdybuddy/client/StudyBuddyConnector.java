b17173a687e3868bf7e94ead0206baeb9a28122a
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-03-22 18:38:59 -0400

    Chatroom Color Scheme Corrections
    Fixed the color descrepencies in the chatroom XML files

--- client/app/src/main/java/edu/uncg/studdybuddy/client/StudyBuddyConnector.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/client/StudyBuddyConnector.java
@@ -447,7 +447,6 @@ public class StudyBuddyConnector {
                 } catch (InterruptedException | IOException ex) {
                     ex.printStackTrace();
                 }
-
             }
         }
     }
