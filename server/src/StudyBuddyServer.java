7e8afaf1b194bfc8f48cee3b59ef97782f5bec06
Anthony Ratliff <arratlif@uncg.edu>
2017-03-25 17:02:02 -0400

    Cleaned up code format for the server class.

--- server/src/StudyBuddyServer.java
+++ server/src/StudyBuddyServer.java
@@ -37,7 +37,7 @@ public class StudyBuddyServer extends Thread {
         this.database = new Database();
         this.onlineList = new OnlineClientList();
         this.chatrooms = new Chatrooms();
-        
+        this.buildChatrooms();
         //serverSocket.setSoTimeout(10000);
     }
 
@@ -72,22 +72,24 @@ public class StudyBuddyServer extends Thread {
             }
         }
     }
-    
-    private boolean buildChatrooms(){
+
+    private boolean buildChatrooms() {
         boolean success = false;
         ResultSet allClasses = this.database.returnAllClasses();
         ResultSet students;
         try {
-            while (allClasses.next()){
+            while (allClasses.next()) {
                 this.chatrooms.addChatroom(allClasses.getString(2), allClasses.getString(3), allClasses.getString(4), allClasses.getString(5));
                 students = this.database.returnAllStudents(allClasses.getString(2), allClasses.getString(3));
                 Boolean online;
-                    while (students.next()){
-                        if (students.getInt(9) == 1){
-                            online = true;
-                        } else online = false;
-                        this.chatrooms.addStudent(allClasses.getString(2), allClasses.getString(3), students.getString(4) + " " + students.getString(5), students.getString(2), online, students.getInt(6));
+                while (students.next()) {
+                    if (students.getInt(9) == 1) {
+                        online = true;
+                    } else {
+                        online = false;
                     }
+                    this.chatrooms.addStudent(allClasses.getString(2), allClasses.getString(3), students.getString(4) + " " + students.getString(5), students.getString(2), online, students.getInt(6));
+                }
             }
         } catch (SQLException ex) {
             Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
@@ -118,7 +120,7 @@ public class StudyBuddyServer extends Thread {
             this.con = con;
             this.onlineList = list;
             this.authenticationThread = this;
-            
+
         }
 
         @Override
@@ -131,85 +133,87 @@ public class StudyBuddyServer extends Thread {
             } catch (IOException ex) {
                 Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
             }
-                
+
             try {
-                
-                while (con.isConnected()){
-                    if (loggedIn) break;
-                String authType = (String) inStream.readUTF();
-                String[] pieces = authType.split(":");
-                if (pieces[1].equals("CREATEACCOUNT") && pieces[0].equals("09")) {
-                    this.createAccount(inStream, outStream, pieces);
-                } else if (pieces[1].equals("LOGIN") && pieces[0].equals("01")) {
-                    this.login(inStream, outStream, pieces);
-                } else {
-                    // Invalid response received and session terminated.
-                    outStream.writeUTF("GOODBYE");
-                    Date curDate = new Date();
-                    System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Authentication:: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Authentication Rejected - Incorrect Protocol.  Session Terminated.");
-                    inStream.close();
-                    outStream.close();
-                }
+
+                while (con.isConnected()) {
+                    if (loggedIn) {
+                        break;
+                    }
+                    String authType = (String) inStream.readUTF();
+                    String[] pieces = authType.split(":");
+                    if (pieces[1].equals("CREATEACCOUNT") && pieces[0].equals("09")) {
+                        this.createAccount(inStream, outStream, pieces);
+                    } else if (pieces[1].equals("LOGIN") && pieces[0].equals("01")) {
+                        this.login(inStream, outStream, pieces);
+                    } else {
+                        // Invalid response received and session terminated.
+                        outStream.writeUTF("GOODBYE");
+                        Date curDate = new Date();
+                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Authentication:: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Authentication Rejected - Incorrect Protocol.  Session Terminated.");
+                        inStream.close();
+                        outStream.close();
+                    }
                 }
             } catch (IOException ex) {
-                if (con.isConnected()){
-                Date curDate = new Date();
-                System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::INVALID:::: Packet from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Session Terminated.");
-                this.authenticationThread.stop();
+                if (con.isConnected()) {
+                    Date curDate = new Date();
+                    System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::INVALID:::: Packet from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Session Terminated.");
+                    this.authenticationThread.stop();
                 }
             } finally {
-               // try {
-                    //inStream.close();
-                    //outStream.close();
-               // } catch (IOException ex) {
-                    //Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
+                // try {
+                //inStream.close();
+                //outStream.close();
+                // } catch (IOException ex) {
+                //Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
                 //}
             }
         }
 
         private void createAccount(DataInputStream inStream, DataOutputStream outStream, String[] pieces) {
-            if (!pieces[2].equals("") && !pieces[3].equals("") && !pieces[4].equals("") && !pieces[5].equals("") && !pieces[6].equals("")){
+            if (!pieces[2].equals("") && !pieces[3].equals("") && !pieces[4].equals("") && !pieces[5].equals("") && !pieces[6].equals("")) {
                 try {
-                Date curDate = new Date();
-                switch (database.registerUser(pieces[2], pieces[3], pieces[5], pieces[6])){
-                    case 0: {
-                        outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":ACCEPTED:00::00");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Accepted - New user added.");
-                        break;
+                    Date curDate = new Date();
+                    switch (database.registerUser(pieces[2], pieces[3], pieces[5], pieces[6])) {
+                        case 0: {
+                            outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":ACCEPTED:00::00");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Accepted - New user added.");
+                            break;
+                        }
+                        case 1: {
+                            outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:01::00");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - User with email already exists.");
+                            break;
+                        }
+                        case 2: {
+                            outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:02::00");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Incorrect password format.");
+                            break;
+                        }
+                        case 3: {
+                            outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:03::00");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Incorrect email format.");
+                            break;
+                        }
+                        case 4: {
+                            outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:04::00");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Unknown database error.");
+                            break;
+                        }
+                        default: {
+                            outStream.writeUTF("GOODBYE");
+                            System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::INVALID:::: Packet from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Session Terminated.");
+                            inStream.close();
+                            outStream.close();
+                            this.authenticationThread.stop();
+                        }
                     }
-                    case 1: {
-                        outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:01::00");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - User with email already exists.");
-                        break;
-                    }
-                    case 2: {
-                        outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:02::00");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Incorrect password format.");
-                        break;
-                    }
-                    case 3: {
-                        outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:03::00");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Incorrect email format.");
-                        break;
-                    }
-                    case 4: {
-                        outStream.writeUTF("09:CREATEACCOUNT:" + pieces[2] + ":REJECTED:04::00");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Register:::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Registration Rejected - Unknown database error.");
-                        break;
-                    }
-                    default: {
-                        outStream.writeUTF("GOODBYE");
-                        System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::INVALID:::: Packet from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Session Terminated.");
-                        inStream.close();
-                        outStream.close();
-                        this.authenticationThread.stop();
-                    }
+                } catch (IOException ex) {
+                    Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
                 }
-            }catch (IOException ex) {
-                        Logger.getLogger(StudyBuddyServer.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-            
-        }
+
+            }
         }
 
         private void login(DataInputStream inStream, DataOutputStream outStream, String[] pieces) {
@@ -236,8 +240,8 @@ public class StudyBuddyServer extends Thread {
                             System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Login:::::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Login Rejected - No such user.");
                             attempts++;
                             if (attempts != 3) {
-                                    String reply = "01:LOGIN:" + pieces[2] + ":" + pieces[3] + ":01:NOUSER:00";
-                                    outStream.writeUTF(reply);
+                                String reply = "01:LOGIN:" + pieces[2] + ":" + pieces[3] + ":01:NOUSER:00";
+                                outStream.writeUTF(reply);
                             }
                         } else if (result.getString("pass_word").equals(passWord)) {
                             Date curDate = new Date();
@@ -254,7 +258,7 @@ public class StudyBuddyServer extends Thread {
                             String ip = con.getRemoteSocketAddress().toString().substring(1);
                             int stat = database.getUserStatus(userName);
                             onlineList.addClient(first + " " + last, mail, ip, stat, inFromClient, outToClient, sess);
-                            
+
                             session.start();
                             this.loggedIn = true;
                             break;
@@ -263,10 +267,10 @@ public class StudyBuddyServer extends Thread {
                             System.out.println("RECEIVED: " + DateFormat.getInstance().format(curDate) + "  ::Login:::::: Request from: " + con.getRemoteSocketAddress().toString().substring(1) + " - Login Rejected - Incorrect password.");
                             attempts++;
                             if (attempts != 3) {
-                                    String reply = "01:LOGIN:" + pieces[2] + ":" + pieces[3] + ":02:BADPASS:00";
-                                    outStream.writeUTF(reply);
-                                    
-                                }
+                                String reply = "01:LOGIN:" + pieces[2] + ":" + pieces[3] + ":02:BADPASS:00";
+                                outStream.writeUTF(reply);
+
+                            }
                         }
                         if (attempts == 3) {
                             Date curDate = new Date();
