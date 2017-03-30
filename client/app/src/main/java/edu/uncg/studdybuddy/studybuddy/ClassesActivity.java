ea74ea67a02a9c1013e962948fafa79deb4ac536
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-03-29 23:14:27 -0400

    Classes GUI
    set up the adapter to display clases

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ClassesActivity.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ClassesActivity.java
@@ -2,12 +2,40 @@ package edu.uncg.studdybuddy.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import StudyBuddy.Chatrooms;
+import butterknife.InjectView;
 
 public class ClassesActivity extends AppCompatActivity {
+    Chatrooms classes;
+    private ListView classList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classes);
+
+        classList = (ListView) findViewById(R.id.list_classes);
+
+        Bundle bundle = getIntent().getExtras();
+        classes = (Chatrooms) bundle.getSerializable("classes");
+
+        List<String> arrayList = new ArrayList<String>();
+        arrayList.add(classes.getClass().toString());
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, arrayList );
+
+        classList.setAdapter(arrayAdapter);
+        //ListView listView = (ListView) findViewById()
     }
+
+
+
 }
