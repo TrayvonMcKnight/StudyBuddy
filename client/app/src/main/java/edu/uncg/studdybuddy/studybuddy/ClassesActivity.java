cf7aa248d9b01ca09a71c7970524fd7b43f513be
Anthony Ratliff <arratlif@uncg.edu>
2017-04-01 20:50:20 -0400

    Clientside:  Classes activity now correctly populates with class names for the student logged in.

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ClassesActivity.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ClassesActivity.java
@@ -7,6 +7,7 @@ import android.widget.ListView;
 import java.util.ArrayList;
 import java.util.List;
 import StudyBuddy.Chatrooms;
+import edu.uncg.studdybuddy.client.StudyBuddyConnector;
 
 public class ClassesActivity extends AppCompatActivity {
     Chatrooms classes;
@@ -19,12 +20,17 @@ public class ClassesActivity extends AppCompatActivity {
 
         classList = (ListView) findViewById(R.id.list_classes);
 
-        Bundle bundle = getIntent().getExtras();
-        classes = (Chatrooms) bundle.getSerializable("classes");
+        StudyBuddyConnector connector = StartActivity.server.getInstance();
+        classes = connector.getChatrooms();
+        String[] classArray = classes.getClassNamesAndSection();
 
         List<String> arrayList = new ArrayList<String>();
-        arrayList.add(classes.getClass().toString());
-
+        List<String> arrayList2 = new ArrayList<String>();
+        for (int c = 0;c < classArray.length;c++) {
+            String[] pieces = classArray[c].split(":");
+            arrayList.add(pieces[0] + "-" + pieces[1]);
+            arrayList2.add(classes.getProfessorName(pieces[0], pieces[1]));
+        }
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, arrayList );
 
