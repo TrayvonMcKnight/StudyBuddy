16ecccf62e816fe680929afbcb444a9d92a847db
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-04-02 02:55:48 -0400

    Created Listview.
    Added change password back into settings. Needs to add serverconnection

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/SettingsActivity.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/SettingsActivity.java
@@ -1,13 +1,33 @@
 package edu.uncg.studdybuddy.studybuddy;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    List<String> settingsOptions = new ArrayList<>();
+    ListView listView = (ListView)findViewById(R.id.list_settings);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settingsOptions.add("Change Password");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, settingsOptions);
+
+        listView.setAdapter(arrayAdapter);
+
     }
 }
