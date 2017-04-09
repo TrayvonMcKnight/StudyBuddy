4bcaaa145f3330582c2f82abc9991abda36a8311
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-04-09 01:48:43 -0400

    Added try_again button to splash screen for when the server fails

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/SplashActivity.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/SplashActivity.java
@@ -4,22 +4,28 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import butterknife.InjectView;
 import edu.uncg.studdybuddy.client.StudyBuddyConnector;
 
 /**
  * Created by Metalaxe on 3/5/2017.
  */
 
-public class SplashActivity extends ActionBarActivity {
+public class SplashActivity extends AppCompatActivity {
     private final String VERSION = "1.00";
     TextView versionText;
     TextView statusText;
     private Handler Handler1 = new Handler();
     private Handler Handler2 = new Handler();
     private StudyBuddyConnector connector;
+    @InjectView(R.id.tryAgainbtn) Button try_again;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -51,6 +57,14 @@ public class SplashActivity extends ActionBarActivity {
         this.versionText.setText("Study Buddy Version: " + VERSION);
         this.statusText.setText("Looking For Server...");
 
+        try_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StartActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     public void openLogin() {
@@ -68,6 +82,8 @@ public class SplashActivity extends ActionBarActivity {
                 }
                 default: {
                     Toast.makeText(getBaseContext(), "Server Unavailable", Toast.LENGTH_LONG).show();
+                    try_again.setVisibility(View.VISIBLE);
+
                 }
             }
         }
