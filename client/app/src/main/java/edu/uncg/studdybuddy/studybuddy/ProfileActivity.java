ace201521a34e0a5e7df05199041e14e4702a3e7
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-04-19 23:07:44 -0400

    Displaying common classes between students

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ProfileActivity.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/ProfileActivity.java
@@ -1,7 +1,11 @@
 package edu.uncg.studdybuddy.studybuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -29,10 +33,10 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
         ButterKnife.inject(this);
-
         Bundle extras = getIntent().getExtras();
-
+        common_classList = (ListView) findViewById(R.id.common_classes_list);
         StudyBuddyConnector connector = StartActivity.server.getInstance();
+
         classes = connector.getChatrooms();
         final String[] classArray = classes.getClassNamesAndSection();
 
@@ -42,13 +46,13 @@ public class ProfileActivity extends AppCompatActivity {
 
         student1_classes = compareClasses(pieces, classes, extras.get("otherEmail").toString());
 
-        common_classList = (ListView) findViewById(R.id.common_classes_list);
-
         studentName.setText(extras.get("otherName").toString());
         studentEmail.setText(extras.get("otherEmail").toString());
 
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1,student1_classes);
 
-
+        common_classList.setAdapter(arrayAdapter);
     }
 
     private ArrayList<String> compareClasses(String[] classPieces, Chatrooms sClasses, String email){
