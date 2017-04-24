dd2e95b354d9f837c8eb83052afecf2fb981d04e
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-04-24 15:25:34 -0400

    Basic Profile Almost complete

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/BasicProfile.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/BasicProfile.java
@@ -2,12 +2,23 @@ package edu.uncg.studdybuddy.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.InjectView;
 
 public class BasicProfile extends AppCompatActivity {
+
+    @InjectView(R.id.displayName) TextView displayName;
+    @InjectView(R.id.displayEmail) TextView displayEmail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_profile);
+
+        Bundle extras = getIntent().getExtras();
+
+        displayName.setText("");
+        displayEmail.setText("");
     }
 }
