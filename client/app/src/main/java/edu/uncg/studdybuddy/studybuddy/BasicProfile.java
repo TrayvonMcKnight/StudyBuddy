c1b22a01849bfc8f2f1f69c2640a9e8125f02a8f
Trayvon McKnight <mcknighttrayvon@gmail.com>
2017-04-24 19:38:09 -0400

    Profile Button Now Operational

--- client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/BasicProfile.java
+++ client/app/src/main/java/edu/uncg/studdybuddy/studybuddy/BasicProfile.java
@@ -4,9 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import StudyBuddy.Student;
+import butterknife.ButterKnife;
 import butterknife.InjectView;
 
 public class BasicProfile extends AppCompatActivity {
+
 
     @InjectView(R.id.displayName) TextView displayName;
     @InjectView(R.id.displayEmail) TextView displayEmail;
@@ -15,10 +18,9 @@ public class BasicProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_profile);
+        ButterKnife.inject(this);
 
-        Bundle extras = getIntent().getExtras();
-
-        displayName.setText("");
-        displayEmail.setText("");
+        displayName.setText(StartActivity.server.getUserName());
+        displayEmail.setText(StartActivity.server.getUserEmail());
     }
 }
